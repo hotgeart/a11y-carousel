ce06a41c21f02b994275090d0e83ffcb18329e68
thomasmester <thomas@blue4you.be>
2020-03-02 16:19:02 +0100

    swipe gesture dist folder

--- src/a11y-carousel.js
+++ src/a11y-carousel.js
@@ -1,17 +1,22 @@
 //@prepros-prepend lib/Array.from.js
+//@prepros-prepend lib/Debounce.js
 //@prepros-prepend lib/NodeList.forEach.js
-//@prepros-prepend lib/Element.dataset.js
+//@prepros-prepend lib/TinyGesture.js
 
 class A11yCarousel {
   constructor(selector, userSettings) {
     // Default Settings of the Slide show
     const defaultSettings = {
       autoplay: true,
-      autoplaySpeed: 5000,
+      autoplaySpeed: 1000,
       transitionSpeed: 300,
-      arrows: false,
+      arrows: true,
       dots: true,
       sliderIndex: 0,
+      width: `auto`,
+      height: `auto`,
+      gesture: true,
+      pauseOnHover: true,
       className: `custom-class`,
       ariaRoledescription: `carousel`,
       ariaLabel: `Slideshow description`,
@@ -34,8 +39,7 @@ class A11yCarousel {
     this._prevId = `${this._selector.slice(1)}-prev`;
     this._nextId = `${this._selector.slice(1)}-next`;
     this._sliderIndex = this.getSettings().sliderIndex;
-    this._isPlaying = true;
-    this._canClick = true;
+    this._isPlaying = this.getSettings().autoplay;
 
     // Create a wrapper for the whole slide show
     const sliderWrapper = document.createElement(`div`);
@@ -56,11 +60,11 @@ class A11yCarousel {
 
     // Set attribute and size
     this._element.style.position = `relative`;
-    this._element.setAttribute(`aria-live`, `off`);
-    this.initSlidesHeight();
+    this._element.setAttribute(`aria-live`, (this.getSettings().autoplay) ? 'off' : 'polite');
+    this.initSlidesSize();
 
     window.addEventListener(`resize`, () => {
-      this.setHeight();
+      this.setSize();
     });
 
     // Set all the slides
@@ -89,11 +93,11 @@ class A11yCarousel {
     /* DOTS */
     if (this.getSettings().dots) {
       // Create a wrapper for the dots
-      const dotsWrapper = document.createElement(`div`);
-      dotsWrapper.classList.add(`dots`);
-      dotsWrapper.setAttribute(`role`, `tablist`);
-      dotsWrapper.setAttribute(`aria-label`, this.getSettings().tablistText);
-      sliderWrapper.insertAdjacentElement(`afterbegin`, dotsWrapper);
+      this.dotsWrapper = document.createElement(`div`);
+      this.dotsWrapper.classList.add(`dots`);
+      this.dotsWrapper.setAttribute(`role`, `tablist`);
+      this.dotsWrapper.setAttribute(`aria-label`, this.getSettings().tablistText);
+      sliderWrapper.insertAdjacentElement(`afterbegin`, this.dotsWrapper);
 
       this._slides.forEach((item, index) => {
         // Create a button dot
@@ -106,12 +110,16 @@ class A11yCarousel {
         dotButton.setAttribute(`slider-id`, index);
         dotButton.setAttribute(`role`, `tab`);
         dotButton.setAttribute(`tabindex`, -1);
+
+        if (index == this._sliderIndex) {
+          dotButton.classList.add(`active`);
+        }
         // the first one need to be accessible via [TAB]
         if (index == 0) {
           dotButton.setAttribute(`tabindex`, 0);
         }
         dotButton.textContent = `${this.getSettings().slideText} ${index + 1}`;
-        dotsWrapper.insertAdjacentElement(`beforeend`, dotButton);
+        this.dotsWrapper.insertAdjacentElement(`beforeend`, dotButton);
 
         // Event Listeners dots click
         dotButton.addEventListener(`click`, e => {
@@ -121,28 +129,28 @@ class A11yCarousel {
         // Event Listeners dots keyboard arrows
         dotButton.addEventListener(`keydown`, e => {
           // ->
-          if (e.which == 39 && this._canClick) {
+          if (e.which == 39) {
             if (e.currentTarget.nextSibling !== null) {
               e.currentTarget.nextSibling.focus();
               this.setSlider(e.currentTarget.nextSibling.getAttribute(`slider-id`));
             }
           }
           // <-
-          if (e.which == 37 && this._canClick) {
+          if (e.which == 37) {
             if (e.currentTarget.previousSibling !== null) {
               e.currentTarget.previousSibling.focus();
               this.setSlider(e.currentTarget.previousSibling.getAttribute(`slider-id`));
             }
           }
           // HOME
-          if (e.which == 36 && this._canClick) {
+          if (e.which == 36) {
             e.currentTarget.parentNode.childNodes[0].focus();
             this.setSlider(
               e.currentTarget.parentNode.childNodes[0].getAttribute(`slider-id`)
             );
           }
           // END
-          if (e.which == 35 && this._canClick) {
+          if (e.which == 35) {
             e.currentTarget.parentNode.childNodes[
               e.currentTarget.parentNode.childNodes.length - 1
             ].focus();
@@ -202,12 +210,13 @@ class A11yCarousel {
     }
 
     // Event Listeners controls
-    document.addEventListener(`click`, e => {
+    document.addEventListener(`click`, debounce ((e) => {
       // play
       if (e.target && e.target.id == this._playId) {
         // Toggle classes for styling
         playBtn.classList.toggle(`playing`);
         playBtn.classList.toggle(`paused`);
+        this._element.setAttribute(`aria-live`, (this._element.getAttribute(`aria-live`) == `polite`) ? `off` : `polite`);
 
         if (this._isPlaying) {
           clearInterval(autoPlayInterval);
@@ -230,84 +239,134 @@ class A11yCarousel {
       // next
       else if (e.target && e.target.id == this._nextId) {
         this.setSlider(this._sliderIndex + 1);
-      } else {
-      }
-    });
+      } else {}
+    }, 200));
+
+    // Gesture / Touch listener
+    if (this.getSettings().gesture) {
+      const gesture = new TinyGesture(this._element);
+      // next
+      gesture.on('swiperight', debounce (() => {
+        this.setSlider(this._sliderIndex - 1);
+      }, 200));
+      // prev
+      gesture.on('swipeleft', debounce (() => {
+        this.setSlider(this._sliderIndex + 1);
+      }, 200));
+    }
+
+    // Pause Hover
+    if (this.getSettings().pauseOnHover) {
+      sliderWrapper.addEventListener(`mouseenter`, () => {
+        clearInterval(autoPlayInterval);
+        this._element.setAttribute(`aria-live`, `polite`);
+      });
+      sliderWrapper.addEventListener(`mouseleave`, () => {
+        if (this._isPlaying) {
+          // TODO : Create a function ?
+          // restart autoplay
+          window.autoPlayInterval = setInterval(
+            this.autoPlay,
+            this.getSettings().autoplaySpeed
+          );
+          this._element.setAttribute(`aria-live`, `off`);
+        }
+      });
+    }
   }
 
   /*
    * Select a slide
    */
-  setSlider = (sliderIndex = 0) => {
-    if (this._canClick) {
-      this._canClick = false;
-      // If current slider do nothing
-      if (this._sliderIndex == sliderIndex) {
-        return null;
-      }
-      // If last slider, got to the first one
-      if (sliderIndex >= this._slides.length) {
-        sliderIndex = 0;
-      }
-      // If first slider, got to the last one
-      if (sliderIndex < 0) {
-        sliderIndex = this._slides.length - 1;
-      }
-
-      // Annimation
-      const oldIndex = this._sliderIndex;
-      const newIndex = sliderIndex;
-      this._sliderIndex = sliderIndex;
-
-      this._slides[oldIndex].style.opacity = 0;
-      this._slides[oldIndex].style.zIndex = 2;
-      this._slides[newIndex].style.zIndex = 1;
-      this._slides[newIndex].style.display = `block`;
-
-      // TODO : Create a function ?
-      // restart autoplay
-      if (this.getSettings().autoPlay && this._isPlaying) {
-        clearInterval(autoPlayInterval);
-        window.autoPlayInterval = setInterval(
-          this.autoPlay,
-          this.getSettings().autoplaySpeed
-        );
-      }
-
-      // When the Annimation is over
-      setTimeout(() => {
-        this._slides[oldIndex].style.zIndex = 1;
-        this._slides[newIndex].style.zIndex = 2;
-        this._slides[oldIndex].style.opacity = null;
-        this._slides[oldIndex].style.display = `none`;
-        this._canClick = true;
-      }, this.getSettings().transitionSpeed + 100); // Need more time than transitionSpeed to avoid annimation glitches
+  setSlider = debounce ((sliderIndex = 0) => {
+    // If current slider do nothing
+    if (this._sliderIndex == sliderIndex) {
+      return null;
     }
-  };
+    // If last slider, got to the first one
+    if (sliderIndex >= this._slides.length) {
+      sliderIndex = 0;
+    }
+    // If first slider, got to the last one
+    if (sliderIndex < 0) {
+      sliderIndex = this._slides.length - 1;
+    }
+
+    // Annimation
+    const oldIndex = this._sliderIndex;
+    const newIndex = sliderIndex;
+    this._sliderIndex = sliderIndex;
+
+    this._slides[oldIndex].style.opacity = 0;
+    this._slides[oldIndex].style.zIndex = 2;
+    this._slides[newIndex].style.zIndex = 1;
+    this._slides[newIndex].style.display = `block`;
+
+    // TODO : Create a function ?
+    // restart autoplay
+    if (this.getSettings().autoPlay && this._isPlaying) {
+      clearInterval(autoPlayInterval);
+      window.autoPlayInterval = setInterval(
+        this.autoPlay,
+        this.getSettings().autoplaySpeed
+      );
+    }
+
+    // Active class for Dot
+    if (this.getSettings().dots) {
+      const dots = Array.from(this.dotsWrapper.children);
+      dots.forEach(item => {
+        if(item.getAttribute(`slider-id`) == newIndex) {
+          item.classList.add(`active`);
+        } else {
+          item.classList.remove(`active`);
+        }
+      })
+    }
+
+    // When the Annimation is over
+    setTimeout(() => {
+      this._slides[oldIndex].style.zIndex = 1;
+      this._slides[newIndex].style.zIndex = 2;
+      this._slides[oldIndex].style.opacity = null;
+      this._slides[oldIndex].style.display = `none`;
+    }, this.getSettings().transitionSpeed + 100); // Need more time than transitionSpeed to avoid annimation glitches
+  }, 200);
 
   /**
-   *  Set the height for the container of the slides
-   *  need to wait the images load
+   *  Functionn called before setSize
+   *  setSize need the images to be fully loaded
    */
-  initSlidesHeight = () => {
+  initSlidesSize = () => {
     const imgs = Array.from(this._element.querySelectorAll(`img`));
 
     if (imgs.length == 0) {
-      this.setHeight();
+      this.setSize();
     }
-    imgs[0].onload = () => {
-      this.setHeight();
-    };
+
+    imgs[imgs.length-1].addEventListener('load', () => {
+      this.setSize();
+    });
   };
 
   /**
-   *
+   * Set the size of the container of the slides
    */
-  setHeight = () => {
-    this._element.style.height = `${
-      this._slides[this._sliderIndex].offsetHeight
-    }px`;
-  };
+  setSize = debounce (() => {
+      if (this.getSettings().width !== `auto`) {
+        this._element.style.width = this.getSettings().height;
+        this._element.style.maxWidth = this.getSettings().height;
+        this._element.style.overflowX = `hidden`;
+      }
+  
+      if (this.getSettings().height !== `auto`) {
+        this._element.style.height = this.getSettings().height;
+        this._element.style.maxHeight = this.getSettings().height;
+        this._element.style.overflowY = `hidden`;
+      } else {
+        this._element.style.height = `${this._slides[this._sliderIndex].offsetSize}px`;
+      }
+  }, 100);
 
   /**
    * function called by the setInterval autoplay
